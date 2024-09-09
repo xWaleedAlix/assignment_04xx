64a0aac102812fb6f500ab7c7f99b843c7ed26a4
Waleed-Ali <waleedali534363279@gmail.com>
2024-09-09 20:11:49 +0500

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 const Header = () => {
   return (
-    <div className='flex justify-evenly bg-slate-600  h-16 p-4  '>
-        <Link href="\">Home</Link>
-        <Link href="\About">About</Link>
-        <Link href="\Address">Address</Link>
-        <Link href="\Contact">Contact</Link>
+    <div className='flex justify-evenly bg-slate-600  h-16 p-4  hover:bg-slate-400 transition   '>
+        <Link className='hover:text-slate-800' href="\">Home</Link>
+        <Link className='hover:text-slate-800' href="\About">About</Link>
+        <Link className='hover:text-slate-800' href="\Address">Address</Link>
+        <Link className='hover:text-slate-800' href="\Contact">Contact</Link>
     </div>
   )
 }
